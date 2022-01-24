f52b6093814d5f74fec41a694f14597d79ec5049
TrottingSpace <90799518+TrottingSpace@users.noreply.github.com>
2022-01-24 01:47:10 +0100

    2022-01-24_01-47

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,6 +1,6 @@
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.getValue
-//import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 //import androidx.compose.runtime.Composable
 import kotlinx.browser.document
 //import org.jetbrains.compose.web.attributes.*
@@ -9,6 +9,16 @@ import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
 //Color.limegreen
+
+fun numToField(num: Int): List<Int> {
+    var i = 0
+    var j = num
+    while ((j - 11) >= 0) {
+        j -= 11
+        i += 1
+    }
+    return listOf(i, j)
+}
 
 fun main() {
     val fieldColor: MutableList<MutableList<CSSColorValue>> = mutableListOf(*(0..10).map { mutableListOf(*(0..10).map { Color.white }.toTypedArray()) }.toTypedArray())
@@ -26,10 +36,18 @@ fun main() {
         fieldColor[allFields[f][0]][allFields[f][1]] = Color.lightgray
     }
 
-    val myTempList: MutableList<Int> = mutableListOf(0)
-    console.log(myTempList.size, "\n")
+    val playerFields: MutableList<MutableList<MutableList<Int>>> = mutableListOf(*(0..4).map { mutableListOf(*(0..55).map { mutableListOf(0, 0) }.toTypedArray()) }.toTypedArray())
+    val playerPathGreen = listOf(10, 6, 17, 28, 39, 50, 51, 52, 53, 54, 65, 76, 75, 74, 73, 72, 83, 94, 105, 116, 115, 114, 103, 92, 81, 70, 69, 68, 67, 66, 55, 44, 45, 46, 47, 48, 37, 26, 15, 4, 5, 16, 27, 38, 49, 60)
+    val playerPathRed = listOf(120, 76, 75, 74, 73, 72, 83, 94, 105, 116, 115, 114, 103, 92, 81, 70, 69, 68, 67, 66, 55, 44, 45, 46, 47, 48, 37, 26, 15, 4, 5, 6, 17, 28, 39, 50, 51, 52, 53, 54, 65, 64, 63, 62, 61, 60)
+    val playerPathBlue = listOf(110, 114, 103, 92, 81, 70, 69, 68, 67, 66, 55, 44, 45, 46, 47, 48, 37, 26, 15, 4, 5, 6, 17, 28, 39, 50, 51, 52, 53, 54, 65, 76, 75, 74, 73, 72, 83, 94, 105, 116, 115, 104, 93, 82, 71, 60)
+    val playerPathYellow = listOf(0, 44, 45, 46, 47, 48, 37, 26, 15, 4, 5, 6, 17, 28, 39, 50, 51, 52, 53, 54, 65, 76, 75, 74, 73, 72, 83, 94, 105, 116, 115, 114, 103, 92, 81, 70, 69, 68, 67, 66, 55, 56, 57, 58, 59, 60)
 
 
+
+    var currentPlayer by mutableStateOf(1)
+    var tempNum = 0
+
+    //that stays here
     val sudokuDiv = document.getElementById("ManDontGetAngry_root")
     sudokuDiv?.setAttribute("style", "padding: 0px; border: none; aspect-ratio: 1;")
 
@@ -51,8 +69,22 @@ fun main() {
                 for (i in 0..10) {
                     Tr {
                         for (j in 0..10) {
-                            Td({ style { backgroundColor(fieldColor[i][j]) } }) {
-                                Text((j+(i*11)).toString())
+                            Td({
+                                style { backgroundColor(fieldColor[i][j]); if (i==5 && j==5) { backgroundColor(Color.dimgray) } }
+                                if (i==5 && j==5) {
+                                    onClick {
+                                        currentPlayer += 1
+                                        if (currentPlayer >= 5) { currentPlayer = 1 }
+                                    }
+                                }
+                                tempNum = j + (i * 11)
+                            }) {
+                                if (i==5 && j==5) {
+                                    Text(currentPlayer.toString())
+                                }
+                                else {
+                                    Text("$tempNum  ${numToField(tempNum)}")
+                                }
                             }
                         }
                     }
