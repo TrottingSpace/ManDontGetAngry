34d8d081d5df3d0fc7b3c9b3b76e3461ce34e796
TrottingSpace <90799518+TrottingSpace@users.noreply.github.com>
2022-01-25 15:31:23 +0100

    2022-01-25_15-31

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -20,8 +20,16 @@ fun numToField(num: Int): List<Int> {
     return listOf(i, j)
 }
 
+//pieceList[player 0-3 (green, red, blue, yellow)][piece 0-3][properties: 0-position]
+val piecesList: MutableList<MutableList<MutableList<Int>>> = mutableListOf(*(0..3).map { mutableListOf(*(0..3).map { mutableListOf(0, 0, 0) }.toTypedArray()) }.toTypedArray())
+
+fun pieceMove(player: Int, piece: Int) {
+    //
+}
+
 fun main() {
     val fieldColor: MutableList<MutableList<CSSColorValue>> = mutableListOf(*(0..10).map { mutableListOf(*(0..10).map { Color.white }.toTypedArray()) }.toTypedArray())
+    val borderColor: MutableList<MutableList<CSSColorValue>> = mutableListOf(*(0..10).map { mutableListOf(*(0..10).map { Color.transparent }.toTypedArray()) }.toTypedArray())
 
     val allFields: MutableList<MutableList<Int>> = mutableListOf(*(0..120).map { mutableListOf(0, 1) }.toTypedArray())
     for (i in 0..10) {
@@ -32,8 +40,26 @@ fun main() {
     }
     console.log(allFields.toString())
 
+    //coloring fields
     for (f in mutableListOf(4, 5, 6, 15, 17, 26, 28, 37, 39, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 55, 65, 66, 67, 68, 69, 70, 72, 73, 74, 75, 76, 81, 83, 92, 94, 103, 105, 114, 115, 116)) {
         fieldColor[allFields[f][0]][allFields[f][1]] = Color.lightgray
+        borderColor[allFields[f][0]][allFields[f][1]] = Color.dimgray
+    }
+    for (f in mutableListOf(6, 19, 20, 30, 31, 16, 27, 38, 49)) {
+        fieldColor[allFields[f][0]][allFields[f][1]] = Color.limegreen
+        borderColor[allFields[f][0]][allFields[f][1]] = Color.dimgray
+    }
+    for (f in mutableListOf(76, 96, 97, 107, 108, 61, 62, 63, 64)) {
+        fieldColor[allFields[f][0]][allFields[f][1]] = Color.tomato
+        borderColor[allFields[f][0]][allFields[f][1]] = Color.dimgray
+    }
+    for (f in mutableListOf(114, 89, 90, 100, 101, 71, 82, 93, 104)) {
+        fieldColor[allFields[f][0]][allFields[f][1]] = Color.dodgerblue
+        borderColor[allFields[f][0]][allFields[f][1]] = Color.dimgray
+    }
+    for (f in mutableListOf(44, 12, 13, 23, 24, 56, 57, 58, 59)) {
+        fieldColor[allFields[f][0]][allFields[f][1]] = Color.gold
+        borderColor[allFields[f][0]][allFields[f][1]] = Color.dimgray
     }
 
     val playerFields: MutableList<MutableList<MutableList<Int>>> = mutableListOf(*(0..4).map { mutableListOf(*(0..55).map { mutableListOf(0, 0) }.toTypedArray()) }.toTypedArray())
@@ -70,7 +96,7 @@ fun main() {
                     Tr {
                         for (j in 0..10) {
                             Td({
-                                style { backgroundColor(fieldColor[i][j]); if (i==5 && j==5) { backgroundColor(Color.dimgray) } }
+                                style { backgroundColor(fieldColor[i][j]); border(1.px, LineStyle.Solid, borderColor[i][j]); if (i==5 && j==5) { backgroundColor(Color.dimgray); border(1.px, LineStyle.Solid, Color.black) } }
                                 if (i==5 && j==5) {
                                     onClick {
                                         currentPlayer += 1
@@ -83,7 +109,7 @@ fun main() {
                                     Text(currentPlayer.toString())
                                 }
                                 else {
-                                    Text("$tempNum  ${numToField(tempNum)}")
+                                    Text("#$tempNum   ${numToField(tempNum)[0]},${numToField(tempNum)[1]}")
                                 }
                             }
                         }
